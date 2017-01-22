58af3cdd9546295b301c3a9ce678963e33d88ce1
moopings <mongkonchai4412@gmail.com>
2017-01-22 12:35:07 +0700

    reformat files in week1

--- week1/problem2.sh
+++ week1/problem2.sh
@@ -1,2 +1,13 @@
 cat /dev/null > allbooks.txt
-sed 's/<[^>]*>//g' <(curl -s http://www.textfiles.com/etext/FICTION) | cut -d ' ' -f1 | grep -E '*.txt' | head -2| while read line; do echo "Loading... $line"; curl -s http://www.textfiles.com/etext/FICTION/$line >> allbooks.txt; done; echo "All books was concatenated."
+
+sed 's/<[^>]*>//g' <(curl -s http://www.textfiles.com/etext/FICTION/) |
+cut -d ' ' -f1 |
+grep -E '*.txt' |
+while read line
+do
+    echo "Loading... $line"
+    curl -s http://www.textfiles.com/etext/FICTION/$line >> allbooks.txt
+    echo "$line was downloaded"
+    echo
+done
+echo "All books was concatenated."
